9d9940c8067a49e860f6af8bf4d3ca4af3cf5b1a
ZacharyHsu <zachary14584@gmail.com>
2020-06-02 21:02:28 +1200

    Beginning of the Project

---
+++ js/Project.js
@@ -0,0 +1,34 @@
+const COUNTRIES = [
+	{
+  	id: "country-1",
+    country_title: "Botswana",
+    country_subtitle: "Summer 2020",
+    country_description: "Spot Hippos and Solve Hypotheses Amid Unspoiled African Scenery. Diamond-rich and conservation friendly, Botswana has devoted over forty percent of its land to national parks, wildlife reserves, and nature preserves, and during our time here we’ll have ample opportunities to engage in some truly unique project-based learning across all three.", 
+    image: "images/Botswana.jpg",
+    image_title: "Botswana animals"
+  }, 
+	{
+  	id: "country-2",
+    country_title: "India",
+    country_subtitle: "Fall 2020",
+    country_description: "Leave your perceptions at the temple door. A country best known for its innumerable deities, mouth-searing dishes, elaborate weddings, and multicolored sarees. Once you venture past these cliches, however, you realize that few places offer as many truly unique experiences to young and eager travelers as India does. Instead of Hyderabad, this time around our term will be based out of Mumbai, India’s most populous and multicultural city.", 
+    image: "images/India.jpg",
+    image_title: "India city"
+  }, 
+	{
+  	id: "country-3",
+    country_title: "Japan",
+    country_subtitle: "Winter 2020",
+    country_description: "Tradition and modernity combine for a timeless experience. Japan can always be counted on to stay much the same. Little will have changed in Hiroshima since our first visit in 2014, and our new students will likely enjoy the same arcades and okonomiyaki stalls as those who preceded them. ",
+   	image: "images/Japan.jpg",
+    image_title: "Japan city"
+  }, 
+	{
+  	id: "country-4",
+    country_title: "Spain",
+    country_subtitle: "Spring 2020",
+    country_description: "The Basque country is a region unlike any other. Spain is one of Europe’s most scenic countries, and, perhaps more importantly, one of its friendliest. Gifted architects such as Antoni Gaudí have crafted many wondrous masterpieces throughout Spain, and many stand side-by-side with well-preserved Roman ruins.", 
+    image: "images/Spain.jpg",
+    image_title: "Spain city"
+  }
+]
